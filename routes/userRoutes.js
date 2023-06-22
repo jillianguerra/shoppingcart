4a2ffcdc9a9fc46e190afbd45553c1e2ccdea741
jillianguerra <guerrajilliant@gmail.com>
2023-06-22 19:18:34 -0400

    sets user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+
+router.post('/new', userController.createUser)
+router.post('/login', userController.loginUser)
+router.post('/logout', userController.auth, userController.logoutUser)
+router.put('/:id', userController.auth, userController.updateUser)
+router.delete('/:id', userController.auth, userController.deleteUser)
