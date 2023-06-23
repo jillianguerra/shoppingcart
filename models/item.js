c11904a64363ec820374b311dc6a2a5c4257440d
jillianguerra <guerrajilliant@gmail.com>
2023-06-22 20:28:44 -0400

    item schema created. Serving up those items.

---
+++ models/item.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: String,
+    price: {type: Number, required: true}
+})
+
+const Item = mongoose.model('Item', itemSchema)
+module.exports = Item
